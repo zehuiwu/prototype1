4ebd2d1674913cf35e455b63ad4c839b94dcc8a5
zehui wu <zehuiwu@usc.edu>
2021-02-09 23:41:35 +0800

    fourth commit

--- prototype2/Assets/script/CheckMoving.cs
+++ prototype2/Assets/script/CheckMoving.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckMoving : MonoBehaviour
 {
     public Rigidbody2D rb;
-
-    
+    [SerializeField] Text Die;
+    private void FixedUpdate()
+    {
+        if (rb.velocity.magnitude == 0)
+        {
+            Die.enabled = true;
+        }
+    }
 
 }
